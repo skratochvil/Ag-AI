328987d2ede223b1af27b1a3b06cdd226179746e
skratochvil <skratochvil@gmail.com>
2020-02-16 16:53:38 -0600

    Added features for number of pixels within "brown" intervals in RGB

--- getBasicFeatures.py
+++ getBasicFeatures.py
@@ -27,6 +27,12 @@ def getHealthyImagePaths():
     return healthyImagePaths
         
 def getBasicFeatures(listOfImagePaths, csvOutFileName):
+    lowRed = 180
+    highRed = 250
+    lowGreen = 160
+    highGreen = 200
+    lowBlue = 150
+    highBlue = 240
     csvOut = []
     for imagePath in listOfImagePaths:
 # =============================================================================
@@ -47,27 +53,42 @@ def getBasicFeatures(listOfImagePaths, csvOutFileName):
 #Splits RGB array into color bands and calculates average pixel intensity for each band
         red = image[0:4000, 0:6000, 0]
         red = numpy.reshape(red, (6000 * 4000))
-        red = numpy.mean(red)
+        redMean = numpy.mean(red)     
 
         green = image[0:4000, 0:6000, 1]
         green = numpy.reshape(green, (6000 * 4000))
-        green = numpy.mean(green)
+        greenMean = numpy.mean(green)
 
         blue = image [0:4000, 0:6000, 2]
         blue = numpy.reshape(blue, (6000 * 4000))
-        blue = numpy.mean(blue)
+        blueMean = numpy.mean(blue)
+        
+# =============================================================================
+# Calculate features: number of pixels in each band with values within a certain range.
+# This color range represents the "brown" of the blights and was determined manually
+# by inspecting individual "blighted" pixels on many of our images using this website: https://pinetools.com/image-color-picker
+# Please note that these threshold values can probably be tuned more.
+# Nice-To-Have: find a way to count pixels for which all three RGB values are within the threshold, which would be better
+# than simply counting the values independently of each other. 
+# This was tried by iterating over each pixel, but it totally destroyed processing time.
+# =============================================================================
+        numBrownRed,bin_edges = numpy.histogram(red, bins=1, range=(lowRed,highRed))
+        numBrownGreen, bin_edges = numpy.histogram(green, bins=1, range=(lowGreen,highGreen))
+        numBrownBlue, bin_edges = numpy.histogram(blue, bins=1, range=(lowBlue,highBlue))
 #Create feature vectors as list of tuples
-        featureVector = (imagePath, gray, red, green, blue)
+        featureVector = (imagePath, gray, redMean, greenMean, blueMean, numBrownRed[0], numBrownGreen[0], numBrownBlue[0])
         csvOut.append(featureVector)
 #Save feature vectors as CSV file        
     csvfile = open(csvOutFileName,'w', newline='')
     obj = csv.writer(csvfile)
-    obj.writerow(('file', 'gray', 'red', 'green', 'blue'))
+    obj.writerow(('file', 'AvgGrayValue', 'AvgRedValue', 'AvgGreenValue', 'AvgBlueValue', 'NumRedBrown', 'NumGreenBrown', 'NumBlueBrown'))
     obj.writerows(csvOut)
+    print("Feature extraction complete.")
 
 #Example usage. 
 #testImages = getHealthyImagePaths()    
 #getBasicFeatures(testImages[0:5], 'csvOut.csv')
+
 
 
 
