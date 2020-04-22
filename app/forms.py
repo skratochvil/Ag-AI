d40a8945897acfc2851ba83b2d29b91c0440c6fc
lduan <45192148+lduan97@users.noreply.github.com>
2020-04-22 16:28:49 -0500

    Fix typo

--- app/forms.py
+++ app/forms.py
@@ -3,5 +3,5 @@ from wtforms import RadioField, SubmitField
 from wtforms.validators import DataRequired
 
 class LabelForm(FlaskForm):
-    choice = RadioField(u'Label', choices=[('H', u'Healthy'), ('B', u'UnHealthy')], validators = [DataRequired()])
+    choice = RadioField(u'Label', choices=[('H', u'Healthy'), ('B', u'Unhealthy')], validators = [DataRequired()])
     submit = SubmitField('Add Label')
