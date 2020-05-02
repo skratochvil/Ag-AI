90bae1e45b6710205fbf6276a0ee3471fa077e50
ornd <48333719+ornd@users.noreply.github.com>
2020-05-02 09:05:26 -0500

    Doxygen Ready

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,8 @@
+"""@package flask_app
+This module handles the web apps and acts as the main driver for running this web application.
+
+"""
 from app import app
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug ='True')
-    
+    app.run(host='127.0.0.1', port=5000, debug ='True')    
