1b7a3cca19a77090f4656e2a40638d672341af37
lduan <45192148+lduan97@users.noreply.github.com>
2020-06-03 17:40:33 -0500

    Update ML_Class.py

--- app/ML_Class.py
+++ app/ML_Class.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
-"""@package ML_Class
-This module contains machine learning and active learning objects.
+"""
+Created on Thu Feb  6 12:54:45 2020
+@author: Donovan
 """
 import json
 from json import JSONEncoder
 
 class ML_Model:
     """
-    This class creates a machine learning model based on the data sent, 
+    This class creates a machine learning model based on the data sent,
     data preprocessing, and type of ml classifier.
-    
+
     """
 
     def __init__(self, train_data, ml_classifier, preprocess):
         """
         This function controls the initial creation of the machine learning model.
-        
+
         Parameters
         ----------
         train_data : pandas DataFrame
@@ -24,7 +25,7 @@ class ML_Model:
             The classifier to be used to create the machine learning model.
         preprocess : Python Function
             The function used to preprocess the data before model creation.
-            
+
         Attributes
         -------
         ml_classifier : classifier object
@@ -40,24 +41,24 @@ class ML_Model:
         """
         self.ml_classifier = ml_classifier
         self.preprocess = preprocess
-        
+
         self.X = train_data.iloc[:,: -1].values
         self.y = train_data.iloc[:, -1].values
 
         self.X = self.preprocess.fit_transform(self.X)
-        
+
         self.ml_model = ml_classifier.fit(self.X, self.y)
-        
+
     def GetKnownPredictions(self, new_data):
         """
-        This function predicts the labels for a new set of data that contains labels. 
-        It returns these predictions and the probability. 
-        
+        This function predicts the labels for a new set of data that contains labels.
+        It returns these predictions and the probability.
+
         Parameters
         ----------
         new_data : pandas DataFrame
             The new data to be labeled.
-            
+
         Returns
         -------
         y_prediction : list
@@ -71,17 +72,17 @@ class ML_Model:
         y_probabilities = self.ml_model.predict_proba(new_data_X)
         y_probabilities = [max(prob) for prob in y_probabilities]
         return y_prediction, max(y_probabilities)
-    
+
     def GetUnknownPredictions(self, new_data_X):
         """
-        This function predicts the labels for a new set of data that does not contains labels. 
-        It returns these predictions and the probability. 
-        
+        This function predicts the labels for a new set of data that does not contains labels.
+        It returns these predictions and the probability.
+
         Parameters
         ----------
         new_data : pandas DataFrame
             The new data to be labeled.
-            
+
         Returns
         -------
         y_prediction : list
@@ -94,18 +95,18 @@ class ML_Model:
         y_probabilities = self.ml_model.predict_proba(new_data_X)
         y_probabilities = [max(prob) for prob in y_probabilities]
         return y_prediction, y_probabilities
-    
+
     def K_fold(self):
         """
-        This function performs a 10-fold cross-validation and returns the accuracies of each fold. 
-            
+        This function performs a 10-fold cross-validation and returns the accuracies of each fold.
+
         Returns
         -------
         accuracies : list
             The 10 accuracy values using 10-fold cross-validation.
         """
         from sklearn.model_selection import cross_val_score
-        accuracies = cross_val_score(self.ml_classifier, self.X, self.y, cv=3)    
+        accuracies = cross_val_score(self.ml_classifier, self.X, self.y, cv=3)
         return accuracies
 
     def infoForProgress(self, train_img_names):
@@ -119,25 +120,43 @@ class ML_Model:
             
         Returns
         -------
-        correct_pic : list
-            List of images that were predicted correctly.
+        health_pic : list
+            List of images that were predicted as healthy.
             
-        incorrect_pic : list
-            List of images that were predicted incorrectly.
+        blight_pic : list
+            List of images that were predicted as unhealthy.
         """
         y_actual = self.y
         y_pic = train_img_names
-        y_pred = self.ml_model.predict(self.X)
-        y_pred = list(y_pred)
-        correct_pic = []
-        incorrect_pic = []
-        for y_idx, y in enumerate(y_actual):
-            if y == y_pred[y_idx]:
-                correct_pic.append(y_pic[y_idx])
-            else:
-                incorrect_pic.append(y_pic[y_idx])
-        return correct_pic, incorrect_pic
-    
+        #y_pred = self.ml_model.predict(self.X)
+        #y_pred = list(y_pred)
+        health_pic = []
+        blight_pic = []
+        if len(y_pic) == 10:
+            y_pic = y_pic[::-1]
+            for y_idx, y in enumerate(y_actual):
+                if y == 'H':
+                    health_pic.append(y_pic[y_idx])
+                elif y == 'B':
+                    blight_pic.append(y_pic[y_idx])
+        else:
+            y_pic_head = y_pic[:10]
+            y_pic_head_rev = y_pic_head[::-1]
+            y_pic_result = y_pic_head_rev
+            y_pic_tail = y_pic[10:]
+            y_pic_tail_length = len(y_pic_tail)
+            for i in range(5,y_pic_tail_length + 5,5):
+                y_pic_tail_rev = y_pic_tail[:5]
+                y_pic_tail_rev = y_pic_tail_rev[::-1]
+                y_pic_result = y_pic_result + y_pic_tail_rev
+                y_pic_tail = y_pic_tail[5:]
+            for y_idx, y in enumerate(y_actual):
+                if y == 'H':
+                    health_pic.append(y_pic_result[y_idx])
+                elif y == 'B':
+                    blight_pic.append(y_pic_result[y_idx])
+        return health_pic, blight_pic
+
     def infoForResults(self, train_img_names, test):
         """
         This function returns the information nessessary to display the final results of the active learning model.
@@ -151,11 +170,11 @@ class ML_Model:
             
         Returns
         -------
-        correct_pic : list
-            List of images that were predicted correctly.
+        health_pic_user : list
+            List of images that were predicted as healthy.
             
-        incorrect_pic : list
-            List of images that were predicted incorrectly.
+        blight_pic_user : list
+            List of images that were predicted as blight.
             
         health_pic : list
             List of images in the test set that are predicted to being healthy.
@@ -163,7 +182,7 @@ class ML_Model:
         blight_pic : list
             List of images in the test set that are predicted to being blighted.
         """
-        correct_pic, incorrect_pic = self.infoForProgress(train_img_names)
+        health_pic_user, blight_pic_user = self.infoForProgress(train_img_names)
         test_pic = list(test.index.values)
         y_pred, y_prob = self.GetUnknownPredictions(test)
         health_pic = []
@@ -172,19 +191,19 @@ class ML_Model:
             if y == 'H':
                 health_pic.append(test_pic[y_idx])
             elif y == 'B':
-                blight_pic.append(test_pic[y_idx])                
-        return correct_pic, incorrect_pic, health_pic, blight_pic
+                blight_pic.append(test_pic[y_idx])
+        return health_pic_user, blight_pic_user, health_pic, blight_pic
 
 class Active_ML_Model:
     """
-    This class creates an active learning model based on the data sent, 
+    This class creates an active learning model based on the data sent,
     data preprocessing, and type of ml classifier.
-    
+
     """
     def __init__(self, data, ml_classifier, preprocess, n_samples = 10):
         """
         This function controls the initial creation of the active learning model.
-        
+
         Parameters
         ----------
         data : pandas DataFrame
@@ -195,7 +214,7 @@ class Active_ML_Model:
             The function used to preprocess the data before model creation.
         n_samples : int
             The number of random samples to be used in the initial model creation.
-            
+
         Attributes
         -------
         ml_classifier : classifier object
@@ -208,13 +227,13 @@ class Active_ML_Model:
             The train set.
         """
         from sklearn.utils import shuffle
-        data = shuffle(data)                                                    
-        self.sample = data.iloc[:n_samples, :]   
-        self.test = data.iloc[n_samples:, :]                                   
+        data = shuffle(data)
+        self.sample = data.iloc[:n_samples, :]
+        self.test = data.iloc[n_samples:, :]
         self.train = None
         self.ml_classifier = ml_classifier
         self.preprocess = preprocess
-    
+
     def Train(self, sample):
         """
         This function trains the innitial ml_model
@@ -222,7 +241,7 @@ class Active_ML_Model:
         ----------
         train : pandas DataFrame
             The training set with labels
-        
+
         Attributes Added
         ----------------
         ml_model : fitted machine learning classifier
@@ -238,14 +257,14 @@ class Active_ML_Model:
     def Continue(self, sampling_method, n_samples = 5):
         """
         This function continues the active learning model to the next step.
-        
+
         Parameters
         ----------
         sampling_method : Python Function
             Determines the next set of samples to send to user.
         n_samples : int
             The number of samplest that should be added the the train set.
-            
+
         Attributes Updated
         -------
         ml_classifier : classifier object
@@ -257,42 +276,60 @@ class Active_ML_Model:
         """
         import pandas as pd
         self.sample, self.test = sampling_method(self.ml_model, n_samples)
-    
+
     def infoForProgress(self):
         """
         This function returns the information nessessary to display the progress of the active learning model.
             
         Returns
         -------
-        correct_pic : list
-            List of images that were predicted correctly.
+        health_pic : list
+            List of images that were predicted as healthy.
             
-        incorrect_pic : list
-            List of images that were predicted incorrectly.
+        blight_pic : list
+            List of images that were predicted as unhealthy.
         """
         y_actual = self.ml_model.train['y_value']
         y_pic = list(self.ml_model.train.index)
-        y_pred, y_prob = self.ml_model.GetKnownPredictions(self.ml_model.train)
-        y_pred = list(y_pred)
-        correct_pic = []
-        incorrect_pic = []
-        for y_idx, y in enumerate(y_actual):
-            if y == y_pred[y_idx]:
-                correct_pic.append(y_pic[y_idx])
-            else:
-                incorrect_pic.append(y_pic[y_idx])
-        return correct_pic, incorrect_pic
-    
+        #y_pred, y_prob = self.ml_model.GetKnownPredictions(self.ml_model.train)
+        #y_pred = list(y_pred)
+        health_pic = []
+        blight_pic = []
+        if len(y_pic) == 10:
+            y_pic = y_pic[::-1]
+            for y_idx, y in enumerate(y_actual):
+                if y == 'H':
+                    health_pic.append(y_pic[y_idx])
+                elif y == 'B':
+                    blight_pic.append(y_pic[y_idx])
+        else:
+            y_pic_head = y_pic[:10]
+            y_pic_head_rev = y_pic_head[::-1]
+            y_pic_result = y_pic_head_rev
+            y_pic_tail = y_pic[10:]
+            y_pic_tail_length = len(y_pic_tail)
+            for i in range(5,y_pic_tail_length + 5,5):
+                y_pic_tail_rev = y_pic_tail[:5]
+                y_pic_tail_rev = y_pic_tail_rev[::-1]
+                y_pic_result = y_pic_result + y_pic_tail_rev
+                y_pic_tail = y_pic_tail[5:]
+            for y_idx, y in enumerate(y_actual):
+                if y == 'H':
+                    health_pic.append(y_pic_result[y_idx])
+                elif y == 'B':
+                    blight_pic.append(y_pic_result[y_idx])
+        return health_pic, blight_pic
+
     def infoForResults(self):
         """
         This function returns the information nessessary to display the final results of the active learning model.
             
         Returns
         -------
-        correct_pic : list
+        health_pic_user : list
             List of images that were predicted correctly.
             
-        incorrect_pic : list
+        blight_pic_user : list
             List of images that were predicted incorrectly.
             
         health_pic : list
@@ -301,7 +338,7 @@ class Active_ML_Model:
         blight_pic : list
             List of images in the test set that are predicted to being blighted.
         """
-        correct_pic, incorrect_pic = self.infoForProgress()
+        health_pic_user, blight_pic_user = self.infoForProgress()
         test_pic = list(self.ml_model.train.idx)
         y_pred, y_prob = self.ml_model.GetUnknownPredictions(self.ml_model.test)
         health_pic = []
@@ -310,9 +347,9 @@ class Active_ML_Model:
             if y == 'H':
                 health_pic.append(test_pic[y_idx])
             elif y == 'B':
-                blight_pic.append(test_pic[y_idx])                
-        return correct_pic, incorrect_pic, health_pic, blight_pic
-    
+                blight_pic.append(test_pic[y_idx])
+        return health_pic_user, blight_pic_user, health_pic, blight_pic
+
 class AL_Encoder(JSONEncoder):
     """
     This class attempts to make the Active_ML_Model JSON serializable.
